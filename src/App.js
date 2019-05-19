30146cd877f7125043744d8654a784bcbeb2d688
Yan C Lin <yonalin1@gmail.com>
2019-05-19 18:34:22 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -1,1 +1,15 @@
-// create your App component here
+mport React, { Component } from 'react';
+
+class App extends Component {
+
+  state = {
+    
+  }
+
+  componentDidMount() {
+    fetch http://api.open-notify.org/astros.json
+  }
+
+  render() {
+    
+  }
